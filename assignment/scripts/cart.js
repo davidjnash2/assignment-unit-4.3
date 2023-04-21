6f9f0d8b57af3968ebb4e9e3aab5a2168bf6eea6
Dave Nash <davidjnash2@gmail.com>
2023-04-21 13:27:29 -0500

    up through half of empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,34 +2,56 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = [];
-function addItem( item ){
-    basket.push( item );
-    let result = false;
+let basket = []; //set basket as empty array
+function addItem( item ){ //name function and add argument
+    basket.push( item ); //command to add each item to basket array
+    let result = false; //set defalut result
+    for (i=0; i<basket.length; i++){ //start loop
+        if (item === basket[i]){ //conditional to check if item has been added to basket array
+        result = true //define result as true if item is added
+        } // end conditional
+    } //end loop
+    return result //result of function
+} //end function
+
+
+console.log(`Basket is ${basket}`); //test addItem function
+console.log('Adding apples (expect true)', addItem('apples')); //test addItem function
+console.log(`Basket is now ${basket}`); //test addItem function
+
+console.log(`Basket is ${basket}`); //test addItem function
+console.log('Adding bananas (expect true)', addItem('bananas')); //test addItem function
+console.log(`Basket is now ${basket}`); //test addItem function
+
+
+console.log(`Basket is ${basket}`); //test addItem function
+console.log('Adding pomegranates (expect true)', addItem('pomegranates')); //test addItem function
+console.log(`Basket is now ${basket}`); //test addItem function
+
+
+console.log(`Basket is ${basket}`); //test addItem function
+console.log('Adding kiwi (expect true)', addItem('kiwi')); //test addItem function
+console.log(`Basket is now ${basket}`); //test addItem function
+
+
+
+function listItems (basket) { //name function and add arguemt of basket array
+    for (i=0; i<basket.length; i++){ //start loop
+        console.log('this is an item in the basket:', basket[i]); //log each item individually
+    }//end loop
+}//end listItems function
+
+listItems(basket); //test listItems function
+
+
+
+function empty(basket){
     for (i=0; i<basket.length; i++){
-    if (item === basket[i]){
-        result = true
+        basket.pop();
     }
+    return basket;
 }
-    return result
-}
+empty(basket);
 
+console.log(`should expect basket array to be empty now ${basket}`);
 
-
-console.log(`Basket is ${basket}`);
-console.log('Adding apples (expect true)', addItem('apples'));
-console.log(`Basket is now ${basket}`);
-
-console.log(`Basket is ${basket}`);
-console.log('Adding bananas (expect true)', addItem('bananas'));
-console.log(`Basket is now ${basket}`);
-
-
-console.log(`Basket is ${basket}`);
-console.log('Adding pomegranates (expect true)', addItem('pomegranates'));
-console.log(`Basket is now ${basket}`);
-
-
-console.log(`Basket is ${basket}`);
-console.log('Adding kiwi (expect true)', addItem('kiwi'));
-console.log(`Basket is now ${basket}`);
